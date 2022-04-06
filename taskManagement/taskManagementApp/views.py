880551249efe031662154a04aac5311ecf97ad36
jorgealiste <j.alistea@gmail.com>
2022-04-06 11:48:46 -0400

    Displaying tasksks in the index page

--- taskManagement/taskManagementApp/views.py
+++ taskManagement/taskManagementApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    all_items = Task.objects.all()
+
+    return render(request, 'index.html', {'all_items': all_items})
