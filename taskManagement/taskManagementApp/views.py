afc88c0d0416c231b96212bae2298947cee30851
jorgealiste <j.alistea@gmail.com>
2022-04-06 12:30:01 -0400

    Create new task

--- taskManagement/taskManagementApp/views.py
+++ taskManagement/taskManagementApp/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New item added!')
+            all_items = Task.objects.all()
+            return render(request, 'index.html', {'all_items': all_items})
+
     all_items = Task.objects.all()
 
     return render(request, 'index.html', {'all_items': all_items})
