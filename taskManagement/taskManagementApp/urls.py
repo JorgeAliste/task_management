e058049ca38b6c191c91b75babc43aad2eb45713
jorgealiste <j.alistea@gmail.com>
2022-04-07 19:35:17 -0400

    Login required to see the tasks list

--- taskManagement/taskManagementApp/urls.py
+++ taskManagement/taskManagementApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tasks', views.task_list, name='task_list'),
     path('delete/<item_id>', views.delete, name='delete'),
     path('tasks/<int:task_pk>', views.tasks, name='task_page')
 ]
--- taskManagement/taskManagementApp/views.py
+++ taskManagement/taskManagementApp/views.py
@@ -2,31 +2,36 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def home(request):
+@login_required
+def task_list(request):
     if request.method == 'POST':
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
             messages.success(request, 'New item added!')
-            all_items = Task.objects.all()
-            return redirect('home')
+            return redirect('task_list')
 
     all_items = Task.objects.all()
 
-    return render(request, 'index.html', {'all_items': all_items})
+    return render(request, 'task_list.html', {'all_items': all_items})
 
 
 def delete(request, item_id):
     item = Task.objects.get(pk=item_id)
     item.delete()
     messages.success(request, 'Item deleted!')
-    return redirect('home')
+    return redirect('task_list')
 
-
+@login_required
 def tasks(request, task_pk):
     task = Task.objects.get(id=task_pk)
 
     return render(request, 'task.html', {'task': task})
+
+
+def home(request):
+    return render(request, 'home.html')
