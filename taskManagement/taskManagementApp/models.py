edac9b084b70b9dbb96c05ad738926ded4890ba2
jorgealiste <j.alistea@gmail.com>
2022-04-08 20:05:53 -0400

    Only the owner of the task is able to see the task

--- taskManagement/taskManagementApp/models.py
+++ taskManagement/taskManagementApp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     task = models.CharField(max_length=30)
     priority = models.CharField(max_length=30)
     completed = models.BooleanField(default=False)
--- taskManagement/taskManagementApp/views.py
+++ taskManagement/taskManagementApp/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
@@ -10,12 +11,15 @@ from django.contrib.auth.decorators import login_required
 def task_list(request):
     if request.method == 'POST':
         form = TaskForm(request.POST or None)
+
         if form.is_valid():
-            form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             messages.success(request, 'New item added!')
             return redirect('task_list')
 
-    all_items = Task.objects.all()
+    all_items = Task.objects.filter(user=request.user)
 
     return render(request, 'task_list.html', {'all_items': all_items})
 
@@ -26,11 +30,16 @@ def delete(request, item_id):
     messages.success(request, 'Item deleted!')
     return redirect('task_list')
 
+
 @login_required
 def tasks(request, task_pk):
     task = Task.objects.get(id=task_pk)
 
-    return render(request, 'task.html', {'task': task})
+    if request.user == task.user:
+
+        return render(request, 'task.html', {'task': task})
+    else:
+        raise PermissionDenied()
 
 
 def home(request):
